b0eb1e5f2790b491d1eed66577cdcaabaae8c3a6
DoukuFui <GuenFui@gmail.com>
2024-04-23 22:05:48 +0700

    ver 1.1

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
-function controlMoving(direction){
-    var top = parseInt(document.getElementById("hero").style.top);
-    var left = parseInt(document.getElementById("hero").style.left);
-    var widthSize = document.getElementById("hero").style.width;
-    var heightSize = document.getElementById("hero").style.height;
-    console.log(left);
-    console.log(widthSize);
-    switch (direction){
-        case 0:
-            if (left > 0) left-=10;
+const img = document.getElementById("hero");
+const obj = new Object(img);
 
+function Object(imageElement){
+    this.imageElement = imageElement;
+    this.left = parseInt(imageElement.style.left);
+    this.top = parseInt(imageElement.style.top);
+    this.size = imageElement.offsetHeight;
+
+    this.getSize = function () {
+        return this.size;
+    }
+
+    console.log(this.size);
+    this.moveRight = function(){
+        this.left += 10;
+        this.imageElement.style.left = this.left + "px";
+    }
+
+    this.moveLeft = function(){
+        this.left -= 10;
+        this.imageElement.style.left = this.left + "px";
+    }
+
+    this.moveBottom = function(){
+        this.top += 10;
+        this.imageElement.style.top = this.top + "px";
+    }
+
+    this.moveTop = function(){
+        this.top -= 10;
+        this.imageElement.style.top = this.top + "px";
+    }
+}
+
+function controlMoving(direction){
+    switch (direction) {
+        case 0:
+            if (obj.left < window.innerWidth - obj.getSize() - 20) obj.moveRight();
             break;
         case 1:
-            if (left < window.innerWidth - widthSize) left+=10;
+            if (obj.top < window.innerHeight - obj.getSize() - 20) obj.moveBottom();
             break;
         case 2:
-            if (top > 0) top-=10;
+            if (obj.left > 0) obj.moveLeft();
             break;
         default:
-            if (top < window.innerHeight - heightSize) top+=10;
+            if (obj.top > 0) obj.moveTop();
             break;
     }
-    console.log(left);
-    console.log(widthSize);
-    document.getElementById("hero").style.left = left + "px";
-    document.getElementById("hero").style.top = top + "px";
 }
